6713ed6108176ce38bdbbf06099a1743d61c8dcc
David McConnell <mcconnelld01+ncpe@gmail.com>
2024-06-05 17:18:11 +0100

    add code to run all methods

---
+++ FullExample/run_all.R
@@ -0,0 +1,30 @@
+# Source code for IS method
+source("../functions.R")
+# Load IPD, and specify priors and time horizon
+source("setup.R")
+
+# Fit curves with MLE
+if(file.exists("./output/mle.sims.RDS"))
+{
+  mle.sims<-readRDS("./output/mle.sims.RDS")
+} else {
+  source("fitMLE.R")
+}
+
+# Fit curves with IS
+if(file.exists("./output/is.sims.RDS"))
+{
+  is.sims<-readRDS("./output/is.sims.RDS")
+} else {
+  source("fitIS.R")
+}
+
+
+# Fit curves with expertsurv
+if(file.exists("./output/exs.sims.RDS"))
+{
+  exs.sims<-readRDS("./output/exs.sims.RDS")
+} else {
+  source("fitexpertsurv.R")
+}
+
